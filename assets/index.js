f099be036e2fde8572d2a1f1f77ff5130878eeb9
Sasha <florenciasantillan.com17@gmail.com>
2024-09-16 20:20:39 -0300

    <h1>Hola</h1>     <script>         console.log (5+4)        // prompt ('Ingrese su nombre');        // alert  ('Esto es una alerta');     </script>

---
+++ assets/index.js
@@ -0,0 +1,11 @@
+console.log()
+let nombre = 'Sasha'
+
+const apellido = 'Santillan'
+
+let edad= prompt ('Ingrese su edad')
+console.log(nombre,apellido)
+console.log("tu edad es" + edad)
+alert('tu edad es ' + edad)
+
+console.log
